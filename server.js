b03f25f027dd93d4e28fbab09fdc4446ba58addb
Charlie <charliecotton04@gmail.com>
2020-01-20 16:20:35 -0500

    folder structure fix

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static("app/public"));
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
