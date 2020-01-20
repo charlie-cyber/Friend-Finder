7295936577914a577b694cc0d65a54c6b1b67f55
Charlie <charliecotton04@gmail.com>
2020-01-20 14:38:47 -0500

    added photos

---
+++ app/data/Friends.js
@@ -0,0 +1,29 @@
+var friends = [
+    {
+        name: "Charlize",
+        photo: "https://www.groundzeroweb.com/wp-content/uploads/2017/02/Hottest-South-African-Models-Charlize-Theron.jpg",
+        scores: [3, 5, 3, 3, 2, 3, 5, 2, 2, 2]
+    }, {
+        name: "Scarlett",
+        photo: "https://qph.fs.quoracdn.net/main-qimg-458e7721eeb1aebef38581fb5ae502ac",
+        scores: [2, 3, 3, 1, 4, 2, 4, 5, 4, 1]
+
+    }, {
+        name: "Jessica",
+        photo: "https://cdn.wealthygorilla.com/wp-content/uploads/2019/02/Hottest-Women-Jessica-Alba-800x533.jpg",
+        scores: [2, 3, 1, 1, 4, 3, 2, 5, 2, 2]
+
+    }, {
+
+        name: "Sophia",
+        photo: "https://i.dailymail.co.uk/i/pix/2015/11/13/17/2E6A9FC700000578-3317202-Bombshell_Sofia_Vergara_43_trailed_behind_Angelina_for_Sexiest_F-a-50_1447435256853.jpg",
+        scores: [2, 2, 3, 2, 3, 5, 5, 3, 3, 2]
+    }, {
+
+        name: "Gal",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Gal_Gadot_by_Gage_Skidmore_2.jpg/1200px-Gal_Gadot_by_Gage_Skidmore_2.jpg",
+        scores: [3, 2, 2, 1, 1, 2, 3, 2, 2, 2]
+    },
+];
+
+module.exports = friends;
